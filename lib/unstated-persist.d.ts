cc73d8ee13b891abc8df6008662b80919e89c166
Milan Lajtoš <milan.lajtos@me.com>
2018-11-14 09:10:39 +0100

    Build

--- lib/unstated-persist.d.ts
+++ lib/unstated-persist.d.ts
@@ -3,7 +3,8 @@ import { Container } from 'unstated'
 type PersistConfig = {
   key: string,
   version: number,
-  storage: any, // @TODO add storage typing
+  storage: any, // @TODO add storage typing,
+  reviver: (key: string, value: any) => any,
 }
 
 export class PersistContainer<State extends object> extends Container<State>{
